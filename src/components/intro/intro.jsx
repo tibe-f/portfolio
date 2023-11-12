7639df1d8b86918eb44992c3b2a4c8502bd91c46
tibe-f <tibe.f.amu@gmail.com>
2023-11-12 09:23:01 +0300

    Removed hire me button

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -12,7 +12,6 @@ const Intro = () => {
           <span className="intro-text">I'm <span className="intro-name">Tibe</span><br /> website developer</span> 
           <p className="intropara">I am skilled web developer and Graphics  <br /> Designer with experience in creating visually appealing websites and Graphics</p>
           <div className="introbtns">
-            <Link><button className="btn">Hire me</button></Link>
             <a href={myCV} download><button className="btn resume">My resume</button></a>
           </div>
          </div>
